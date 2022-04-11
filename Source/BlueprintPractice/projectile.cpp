f8fbaf2d8828064ccd96de27c86b3f2766ad3837
The Mannyac <theyacnnam34@gmail.com>
2022-04-11 16:10:43 -0400

    New weapons
    Added baseline for for crossbow bolt and stickbomb weapons, they still  need fixing up, but I'll handle that later.

--- Source/BlueprintPractice/projectile.cpp
+++ Source/BlueprintPractice/projectile.cpp
@@ -10,12 +10,17 @@ Aprojectile::Aprojectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	
+
 	//Create Components
 	Projectile = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement Component"));
+	Projectile->InitialSpeed = 3000;
+	Projectile->MaxSpeed = 3000;
 
 	//Default Variables
 	PrimaryDamageType = Ubase_dt::StaticClass();
 	baseDamage = 1;
+
+	InitialLifeSpan = 5;
 }
 
 // Called when the game starts or when spawned
