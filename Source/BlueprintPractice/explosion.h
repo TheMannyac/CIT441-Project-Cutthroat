ed85d36d2edebd3c675be111df6bfee29bdac0ba
The Mannyac <theyacnnam34@gmail.com>
2022-04-28 16:26:49 -0400

    Added Combat UI and Refined Weapons

---
+++ Source/BlueprintPractice/explosion.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "projectile.h"
+#include "explosion.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLUEPRINTPRACTICE_API Aexplosion : public Aprojectile
+{
+	GENERATED_BODY()
+	
+};
